384be493cdbb2147f73ce5a6470f6615ac01db56
AaronHepta <aaron.pieperjohanns@heptacom.de>
2026-03-06 12:38:00 +0100

    GO-5 Add default color for projects

--- GelloBackend/models/project_models.py
+++ GelloBackend/models/project_models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='projects')
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, default='')
-    indicatorColor = models.CharField(max_length=20, blank=False)
+    indicatorColor = models.CharField(max_length=20, blank=False, default='#0000FF')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
